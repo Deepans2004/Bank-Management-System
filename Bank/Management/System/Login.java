0d06721fb122c1345cbdbd462a0cd3bab6e58bd3
Deepanshu Tiwari <114825687+Deepans2004@users.noreply.github.com>
2024-05-11 20:12:12 +0530

    first commit

---
+++ Bank/Management/System/Login.java
@@ -0,0 +1,19 @@
+package Bank.Management.System;
+import javax.swing.*;
+
+public class Login extends JFrame {
+
+    Login() {
+
+        setTitle("AUTOMATED    TELLER MACHINE");
+        
+        setSize(800, 480);
+        setVisible(true);
+        setLocation(350, 200);
+    }
+
+    public static void main(String[]args){
+        new Login();
+
+    }
+}
