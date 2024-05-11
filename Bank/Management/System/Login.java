8aa8c65d0452e8daa7017c9115300f899aa7c03d
Deepanshu Tiwari <114825687+Deepans2004@users.noreply.github.com>
2024-05-11 20:19:25 +0530

    new commit

--- Bank/Management/System/Login.java
+++ Bank/Management/System/Login.java
@@ -5,8 +5,10 @@ public class Login extends JFrame {
 
     Login() {
 
-        setTitle("AUTOMATED    TELLER MACHINE");
-        
+        setTitle("AUTOMATED TELLER MACHINE");
+        ImageIcon il=new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));
+        JLabel label= new JLabel(il);
+        add(label);
         setSize(800, 480);
         setVisible(true);
         setLocation(350, 200);
