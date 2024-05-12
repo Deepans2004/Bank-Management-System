a3a9cac762ec3b1f17a8fc2262b81b6113b8fd19
Deepanshu Tiwari <114825687+Deepans2004@users.noreply.github.com>
2024-05-12 15:52:06 +0530

    new commit

---
+++ sql query/create database bankmanagementsystem.sql
@@ -0,0 +1,11 @@
+create database bankmanagementsystem;
+
+show databases;
+
+use bankmanagementsystem;
+
+create table signup(formno varchar(20), name varchar(20),father_name varchar(20),dob varchar(20),gender varchar(20),email varchar(30),Marital_status varchar(20),address varchar(40),city varchar(25),pincode varchar(20),state varchar(25)); 
+
+show tables;
+
+select * from signup;
