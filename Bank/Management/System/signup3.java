0d59f2f10b335fd5c03036215953187a608263ca
Deepanshu Tiwari <114825687+Deepans2004@users.noreply.github.com>
2024-05-16 20:24:24 +0530

    new commit

--- Bank/Management/System/signup3.java
+++ Bank/Management/System/signup3.java
@@ -2,11 +2,14 @@ package Bank.Management.System;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
 
-public class signup3 extends JFrame {
+public class signup3 extends JFrame implements ActionListener {
 
     JRadioButton r1,r2,r3,r4;
     JCheckBox c1,c2,c3,c4,c5,c6,c7;
+    JButton submit,cancel;
 
     signup3(){
         setLayout(null);
@@ -90,11 +93,92 @@ public class signup3 extends JFrame {
         c1.setBackground(Color.white);
         c1.setFont(new Font("Raleway",Font.BOLD,16));
         c1.setBounds(100,500,200,30);
-        add(c1);     
+        add(c1);
+
+        c2=new JCheckBox("Internet Banking");
+        c2.setBackground(Color.white);
+        c2.setFont(new Font("Raleway",Font.BOLD,16));
+        c2.setBounds(350,500,200,30);
+        add(c2);
+
+        c3=new JCheckBox("Mobile Banking");
+        c3.setBackground(Color.white);
+        c3.setFont(new Font("Raleway",Font.BOLD,16));
+        c3.setBounds(100,550,200,30);
+        add(c3);
+
+        c4=new JCheckBox("E-Mail & Sms Alerts");
+        c4.setBackground(Color.white);
+        c4.setFont(new Font("Raleway",Font.BOLD,16));
+        c4.setBounds(350,550,200,30);
+        add(c4);
+
+        c5=new JCheckBox("Cheque Book");
+        c5.setBackground(Color.white);
+        c5.setFont(new Font("Raleway",Font.BOLD,16));
+        c5.setBounds(100,600,200,30);
+        add(c5);
+
+        c6=new JCheckBox("E-statement");
+        c6.setBackground(Color.white);
+        c6.setFont(new Font("Raleway",Font.BOLD,16));
+        c6.setBounds(350,600,200,30);
+        add(c6);
+
+        c7=new JCheckBox("I Hereby declare that the above entered details are correct to the best of my knowledge");
+        c7.setBackground(Color.white);
+        c7.setFont(new Font("Raleway",Font.BOLD,12));
+        c7.setBounds(100,680,600,30);
+        add(c7);
+
+        submit=new JButton("Submit");
+        submit.setBackground(Color.BLACK);
+        submit.setForeground(Color.WHITE);
+        submit.setFont(new Font("Raleway",Font.BOLD,14));
+        submit.setBounds(250,720,100,30);
+        submit.addActionListener(this);
+        add(submit);
+
+        cancel=new JButton("Cancel");
+        cancel.setBackground(Color.BLACK);
+        cancel.setForeground(Color.WHITE);
+        cancel.setFont(new Font("Raleway",Font.BOLD,14));
+        cancel.setBounds(420,720,100,30);
+        add(cancel);
+
+        getContentPane().setBackground(Color.white);
 
         setSize(850,820);
         setLocation(350,0);
         setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent ae){
+        if(ae.getSource()==submit){
+            String accounttype=null;
+            if(r1.isSelected()){
+                accounttype="Saving Account";
+            }
+            else if(r2.isSelected()){
+                accounttype="Fixed Deposit Account";
+            }
+            else if(r3.isSelected()){
+                accounttype="Current Account";
+            }
+            else if(r4.isSelected()){
+                accounttype="Recurring deposit Account";
+            }
+
+            Random random=new Random();
+            String  cardnumber=""+Math.abs(random.nextLong()%90000000L)+5040936000000000L;
+
+            String pinnumber=""+Math.abs(random.nextLong()%9000L)+1000L;
+
+            String
+        }
+        else if(ae.getSource()==cancel){
+
+        }
     }
 
     public static void main(String[]args){
