10480e6b3f8211acffd3a1800645801f39b8c0ef
Deepanshu Tiwari <114825687+Deepans2004@users.noreply.github.com>
2024-05-18 21:10:13 +0530

    new commit

--- Bank/Management/System/Transaction.java
+++ Bank/Management/System/Transaction.java
@@ -17,13 +17,42 @@ public class Transaction extends JFrame {
             add(image);
 
             JLabel text=new JLabel("Please Select your Transactions");
-            text.setBounds(235,300,700,35);
+            text.setBounds(210,300,700,35);
             text.setForeground(Color.WHITE);
             text.setFont(new Font("System",Font.BOLD,16));
             image.add(text);
 
+            JButton deposit=new JButton("Deposit");
+            deposit.setBounds(170,415,150,30);
+            image.add(deposit);
+
+            JButton withdrawl=new JButton("Withdrawl");
+            withdrawl.setBounds(355,415,150,30);
+            image.add(withdrawl);
+
+            JButton fastcash=new JButton("Fast cash");
+            fastcash.setBounds(170,450,150,30);
+            image.add(fastcash);
+
+            JButton mini=new JButton("Mini Statement");
+            mini.setBounds(355,450,150,30);
+            image.add(mini);
+
+            JButton pinchange=new JButton("Pin Change");
+            pinchange.setBounds(170,485,150,30);
+            image.add(pinchange);
+
+            JButton balancewnuiry=new JButton("Balance Enquiry");
+            balancewnuiry.setBounds(355,485,150,30);
+            image.add(balancewnuiry);
+
+            JButton exit=new JButton("Exit");
+            exit.setBounds(355,520,150,30); 
+            image.add(exit);
+
             setSize(900,900);
             setLocation(300,0);
+//            setUndecorated(true);
             setVisible(true);
         }
 
