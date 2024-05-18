8432ad4c8281fdb3932b63e64d21e1a71a3db7c1
Deepanshu Tiwari <114825687+Deepans2004@users.noreply.github.com>
2024-05-18 21:18:16 +0530

    new commit

--- Bank/Management/System/Transaction.java
+++ Bank/Management/System/Transaction.java
@@ -27,30 +27,37 @@ public class Transaction extends JFrame implements ActionListener {
 
             deposit=new JButton("Deposit");
             deposit.setBounds(170,415,150,30);
+            deposit.addActionListener(this);
             image.add(deposit);
 
             withdrawl=new JButton("Withdrawl");
             withdrawl.setBounds(355,415,150,30);
+            withdrawl.addActionListener(this);
             image.add(withdrawl);
 
             fastcash=new JButton("Fast cash");
             fastcash.setBounds(170,450,150,30);
+            fastcash.addActionListener(this);
             image.add(fastcash);
 
             mini=new JButton("Mini Statement");
             mini.setBounds(355,450,150,30);
+            mini.addActionListener(this);
             image.add(mini);
 
             pinchange=new JButton("Pin Change");
             pinchange.setBounds(170,485,150,30);
+            pinchange.addActionListener(this);
             image.add(pinchange);
 
             balancewnuiry=new JButton("Balance Enquiry");
             balancewnuiry.setBounds(355,485,150,30);
+            balancewnuiry.addActionListener(this);
             image.add(balancewnuiry);
 
             exit=new JButton("Exit");
             exit.setBounds(355,520,150,30);
+            exit.addActionListener(this);
             image.add(exit);
 
             setSize(900,900);
@@ -60,7 +67,9 @@ public class Transaction extends JFrame implements ActionListener {
         }
 
         public void actionPerformed(ActionEvent ae){
-
+            if(ae.getSource()==exit){
+                System.exit(0);
+            }
         }
 
         public static void main(String args[]){
