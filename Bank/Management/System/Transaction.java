ceb4c50dc68a27ded55b4ea32f81e2ac76bb5eae
Deepanshu Tiwari <114825687+Deepans2004@users.noreply.github.com>
2024-05-18 13:18:02 +0530

    new commit

--- Bank/Management/System/Transaction.java
+++ Bank/Management/System/Transaction.java
@@ -1,8 +1,33 @@
 package Bank.Management.System;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class Transaction extends JFrame {
-    public static void main(String[]args){
 
-    }
+        Transaction() {
+
+            setLayout(null);
+
+            ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
+            Image i2=i1.getImage().getScaledInstance(900,900,Image.SCALE_DEFAULT);
+            ImageIcon i3= new ImageIcon(i2);
+            JLabel image=new JLabel(i3);
+            image.setBounds(0,0,900,900);
+            add(image);
+
+            JLabel text=new JLabel("Please Select your Transactions");
+            text.setBounds(235,300,700,35);
+            text.setForeground(Color.WHITE);
+            text.setFont(new font("System"));
+            image.add(text);
+
+            setSize(900,900);
+            setLocation(300,0);
+            setVisible(true);
+        }
+
+        public static void main(String args[]){
+            new Transaction();
+        }
 }
