c2458682cc8c5d0400b42c15cd57667e661f15ba
Deepanshu Tiwari <114825687+Deepans2004@users.noreply.github.com>
2024-06-11 19:57:28 +0530

    new commit

---
+++ Bank/Management/System/MiniStatement.java
@@ -0,0 +1,19 @@
+package Bank.Management.System;
+
+import javax.swing.*;
+
+public class MiniStatement extends JFrame {
+
+    MiniStatement(){
+        setTitle("Mini Statement");
+
+        setSize(400,600);
+        setLocation(20,20);
+    }
+
+    public static void main(String[]args){
+
+        new MiniStatement();
+
+    }
+}
