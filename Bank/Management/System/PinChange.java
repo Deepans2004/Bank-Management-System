0d66e48b5ab894fdc58c624d7f1cf61773c6b69b
Deepanshu Tiwari <114825687+Deepans2004@users.noreply.github.com>
2024-06-11 13:28:27 +0530

    new commit

--- Bank/Management/System/PinChange.java
+++ Bank/Management/System/PinChange.java
@@ -6,6 +6,10 @@ import java.awt.event.*;
 
 public class PinChange extends JFrame implements ActionListener {
 
+    JTextField pin,repin;
+    JButton change,back;
+
+
     PinChange(String pinchange){
         setLayout(null);
         ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
@@ -27,7 +31,7 @@ public class PinChange extends JFrame implements ActionListener {
         pintext.setBounds(165,320,180,25);
         image.add(pintext);
 
-        JTextField pin=new JTextField();
+        pin=new JTextField();
         pin.setFont(new Font("Raleway",Font.BOLD,25));
         pin.setBounds(330,320,180,25);
         image.add(pin);
@@ -38,17 +42,17 @@ public class PinChange extends JFrame implements ActionListener {
         repintext.setBounds(165,360,180,25);
         image.add(repintext);
 
-        JTextField repin=new JTextField();
+        repin=new JTextField();
         repin.setFont(new Font("Raleway",Font.BOLD,25));
         repin.setBounds(330,360,180,25);
         image.add(repin);
 
-        JButton change=new JButton("CHANGE");
+        change=new JButton("CHANGE");
         change.setBounds(355,485,150,30);
         change.addActionListener(this);
         image.add(change);
 
-        JButton back=new JButton("BACK");
+        back=new JButton("BACK");
         back.setBounds(355,520,150,30);
         back.addActionListener(this);
         image.add(back);
@@ -60,6 +64,36 @@ public class PinChange extends JFrame implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent ae){
+
+        if(ae.getSource()==change){
+        try{
+            String npin=pin.getText();
+            String rpin=repin.getText();
+
+            if(!npin.equals(repin)){
+                JOptionPane.showMessageDialog(null,"Entered pin does not match");
+                return;
+            }
+            if(npin.equals("")){
+                JOptionPane.showMessageDialog(null,"Please enter new PIN");
+                return;
+            }
+
+            if(rpin.equals("")){
+                JOptionPane.showMessageDialog(null,"Please Reenter new pin");
+                return;
+            }
+
+            conn c=new conn();
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+        }else{
+            setVisible(false);
+            new Transaction(pinnumber).setVisible(true);
+        }
 
     }
 
